d9ff53aff0d9b6992c33d82b9e1b424f903a12de
Viko <viko@vikomprenas.com>
2017-02-26 16:55:10 -0600

    initial commit

---
+++ nsndswap/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3 -ttb
+# nsndswap/__main__.py
+# copyright 2017 ViKomprenas, 2-clause BSD license (LICENSE.md)
+
+def main():
+    print('hello')
+
+if __name__ == '__main__':
+    main()
