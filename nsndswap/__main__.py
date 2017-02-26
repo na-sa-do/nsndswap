1bf9836716af1362590dcdbb3d3f74315c544f0e
Viko <viko@vikomprenas.com>
2017-02-26 17:03:09 -0600

    Fetch nsnd correctly

--- nsndswap/__main__.py
+++ nsndswap/__main__.py
@@ -2,8 +2,27 @@
 # nsndswap/__main__.py
 # copyright 2017 ViKomprenas, 2-clause BSD license (LICENSE.md)
 
+import sys
+
 def main():
-    print('hello')
+    nsnd = get_nsnd_page()
+    print(len(nsnd))
+
+def get_nsnd_page():
+    import requests
+    try:
+        req = requests.get('http://xzazupsilon.webs.com/nsnd.html')
+        if req.status_code != 200:
+            sys.stderr.write(f'Request for nsnd returned {req.status_code}, aborting\n')
+            raise SystemExit(1)
+        nsndtext = req.text
+        if len(nsndtext) == 0:
+            sys.stderr.write(f'Got a blank page instead of nsnd, aborting\n')
+            raise SystemExit(1)
+        return nsndtext
+    except Exception as e:
+        sys.stderr.write(f'Caught an exception while fetching nsnd\n')
+        raise
 
 if __name__ == '__main__':
     main()
