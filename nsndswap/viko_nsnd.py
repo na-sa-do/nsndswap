1d8ef1c7a697cf18d2f6860ba1053d6b96c6a446
Viko <viko@vikomprenas.com>
2017-04-24 20:46:03 -0600

    Add Exquisite Corpse to viko_nsnd

--- nsndswap/viko_nsnd.py
+++ nsndswap/viko_nsnd.py
@@ -60,6 +60,9 @@ nsnd = {
     "Elevator #3": ["Jungle #3", "Sburban Elevator"],
     "Another Paradigm": ["Endless Climb", "Rhapsody in Green", "MeGaLoVania", "The Paradox Paradigm", "Maestro", "Sburban Jungle", "Penumbra Phantasm", "Beatdown", "Look Where We Are", "Crystalanthemums", "Heir of Grief", "Showtime (Piano Refrain)", "Revelawesome", "Three in the Morning", "Ruins", "Jungle #3", "Tock", "Unintentional Touhou", "Courser"],
     "Radiation Sickness": ["Gaster's Theme", "Penumbra Phantasm", "Heartache", "Earthsea Borealis", "the rose rap"],
+
+    # The Exquisite Corpse
+    "Exquisite Corpse": ["Carne Vale", "A Taste for Adventure", "Endless Climb", "RCT Title Screen", "Sburban Jungle", "Courser", "Jungle #3", "MeGaLoVania"],
 }
 
 
